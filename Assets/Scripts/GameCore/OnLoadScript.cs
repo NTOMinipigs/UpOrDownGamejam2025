1979d6b5fec6a8a027f9528eab2aa11463083123
Justiks1337 <123255294+Justiks1337@users.noreply.github.com>
2025-09-14 20:11:45 +0400

    FEAT(game, camera): follow camera to character

--- Assets/Scripts/GameCore/OnLoadScript.cs
+++ Assets/Scripts/GameCore/OnLoadScript.cs
@@ -1,4 +1,6 @@
+using Unity.Cinemachine;
 using UnityEngine;
+
 
 namespace DefaultNamespace.GameCore
 {
@@ -7,16 +9,22 @@ namespace DefaultNamespace.GameCore
     /// </summary>
     public class OnLoadScript : MonoBehaviour
     {
+        [SerializeField] private GameObject _camera;
+        
         void Start()
         {
             // Получаем объект с данными из меню
             GameObject menuProxyGameObject = GameObject.Find("MenuProxyObject"); 
             MenuProxyObject menuProxyObject = menuProxyGameObject.GetComponent<MenuProxyObject>();
 
-            // Выполняем скрипт
+            // Ставим персонажа игроку
             Player.Instance.character =
                 Instantiate(menuProxyObject.characterPrefab, new Vector2(0, 0), Quaternion.identity);
 
+            // Подписываем камеру на персонажа
+            _camera.GetComponent<CinemachineCamera>().Follow = Player.Instance.character.transform;
+            _camera.GetComponent<CinemachineCamera>().LookAt = Player.Instance.character.transform;
+            
             // Уничтожаем объекты со сцены, так как он больше не нужен
             Destroy(menuProxyObject);
             Destroy(gameObject);
