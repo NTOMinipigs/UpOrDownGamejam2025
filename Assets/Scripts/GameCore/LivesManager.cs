b81f6bd7b4ecc16b70ba2c76768e14523484d5d6
Justiks1337 <123255294+Justiks1337@users.noreply.github.com>
2025-09-16 00:46:17 +0400

    feat(lives mechanic): add lives mechanic

---
+++ Assets/Scripts/GameCore/LivesManager.cs
@@ -0,0 +1,54 @@
+using DefaultNamespace;
+using UnityEngine;
+
+namespace GameCore
+{
+    /// <summary>
+    /// Менеджер управления жизнями игрока 
+    /// </summary>
+    public class LivesManager : MonoBehaviour
+    {
+        #region UI elements
+
+        [SerializeField] private GameObject _firstHeart;
+        [SerializeField] private GameObject _secondHeart;
+        [SerializeField] private GameObject _thirdHeart;
+
+        #endregion
+
+        /// <summary>
+        /// LivesManager is Singleton, this getter
+        /// </summary>
+        public static LivesManager Instance { private set; get; }
+
+        /// <summary>
+        /// Количество оставшихся жизней
+        /// </summary>
+        private int _lives = 3;
+
+        void Start()
+        {
+            Instance = this;
+        }
+        
+        /// <summary>
+        /// Вызвать при смерти игрока
+        /// </summary>
+        void Dead()
+        {
+            _lives -= 1;
+            if (_lives == 0)
+            {
+                // TODO: Реализовать конец игры
+            }
+
+            // Удаляем жизни из UI
+            if (_lives == 2) _thirdHeart.SetActive(false);
+            else if (_lives == 1) _secondHeart.SetActive(false);
+            
+            // Телепортируем игрока назад
+            StartCoroutine(FadeImage.Instance.FadeScreen());
+            Player.Instance.character.transform.position = Vector3.zero;
+        }
+    }
+}
